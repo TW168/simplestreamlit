ffdf77d5b1ed9e64bc414fdab121ec4321c53f61
Tony <tony.wei@outlook.com>
2022-02-01 22:29:16 -0600

    added some exercise code and changed page title

--- app.py
+++ app.py
@@ -5,6 +5,12 @@ import streamlit as st
 import yfinance as yf
 from datetime import date
 
+# change the page title name
+st.set_page_config(
+   page_title = "simplestreamlit",
+)
+
+
 yf.pdr_override() # <== that's all it takes :-)
 
 # download yahoo finance data to dataframe
@@ -14,3 +20,15 @@ data = pdr.get_data_yahoo(ticker, start="2021-01-01", end=date.today())
 
 st.dataframe(data)
 st.caption('VGT This is caption')
+
+
+col1, col2 = st.columns(2)
+
+with col1:
+    col1.header("Original")
+    st.write ('This column 1')
+
+with col2:
+    col2.header("Grayscale")
+    st.write ('This is column 2')
+
