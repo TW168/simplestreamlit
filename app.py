4fc3694fa4e7d591389655ca4ec16c3b5658ca10
Tony <tony.wei@outlook.com>
2022-02-06 18:18:20 -0600

    First drift

--- app.py
+++ app.py
@@ -1,34 +1,81 @@
-from numpy import datetime64
-from pandas_datareader import data as pdr
+from requests.api import options
 import streamlit as st
-
-import yfinance as yf
-from datetime import date
-
-# change the page title name
-st.set_page_config(
-   page_title = "simplestreamlit",
-)
-
-
-yf.pdr_override() # <== that's all it takes :-)
-
-# download yahoo finance data to dataframe
-ticker= 'VGT'
-
-data = pdr.get_data_yahoo(ticker, start="2021-01-01", end=date.today())
-
-st.dataframe(data)
-st.caption('VGT This is caption')
+import pandas as pd 
+import yfinance as yf 
+import datetime
+import numpy as np 
+from pandas_datareader import data as pdr
+import cufflinks as cf
+from pathlib import Path 
+import statsmodels
+import matplotlib.pyplot as plt
 
 
-col1, col2 = st.columns(2)
+def main():
+    # App title
+    st.markdown('''
+    # Cryptos & Stocks  📈
+   
+    ''')
+    st.write('------')
 
-with col1:
-    col1.header("Original")
-    st.write ('This column 1')
+    # App sidebar
+    st.sidebar.subheader('Parameters')
+    start_date= st.sidebar.date_input('Start date', datetime.date(2020,1,1))
+    end_date= st.sidebar.date_input('End date', datetime.date.today())
 
-with col2:
-    col2.header("Grayscale")
-    st.write ('This is column 2')
+
+    # Download selected tickers from yahoo finance data to dataframe
+    ticker_str= 'BTC-USD', 'ETH-USD', 'DOGE-USD', 'AAPL', 'SPYG', 'SPYD', 'VGT', 'FB'
+
+    @st.cache
+    def load_data(tickers, start_dt, end_dt):
+        """
+        Download stcok history data from Yahoo Finance
+
+        Parameters:
+            tickers : string
+                The string of stocks
+            start_dt : date
+                The start date for the stock history
+            end_dt : date
+                The end date for the sotck history
+			
+        """
+       
+        yf_df = yf.download(ticker_str, group_by='Ticker', start= start_dt, end= end_dt)
+        yf_df = yf_df.stack(level=0).rename_axis(['Date', 'Ticker']).reset_index(level=1)
+     
+        return yf_df
+
+
+    # select the ticker to display
+    data= load_data(ticker_str, start_date, end_date)
+    ticker_selected= st.selectbox('Select a ticker', options= ticker_str)
+    s_data= data[data['Ticker']==ticker_selected]
+    st.write(s_data.describe())
+
+
+    # text for key take away
+    # bbs= Path('BollingerBands.md').read_text()
+
+
+    # # Bollinger bands
+    # st.info('Bollinger Bands®')
+    # st.write('Key Take Away')
+    # st.markdown(bbs, unsafe_allow_html= True)
+
+    qf= cf.QuantFig(s_data, title= 'Bollinger bands', legend= 'top', name= ticker_selected)
+    qf.add_bollinger_bands()
+    fig= qf.iplot(asFigure= True)
+    st.plotly_chart(fig)
+
+
+    st.line_chart(s_data['Close'])
+    st.bar_chart(s_data['Volume'])
+
+    
+
+if __name__ =='__main__':
+    main()
 
